ca5666cc1025f21549825162173f4754e25ae6a6
farooqmajeed <farooq306@hotmail.com>
2017-05-09 13:46:04 +0500

    Api request usin observable.ajax

---
+++ src/containers/Navigation/Navigation.jsx
@@ -0,0 +1,83 @@
+import React, { Component , PropTypes} from 'react';
+import './Navigation.css';
+import * as MUI from 'material-ui'
+import {Link} from 'react-router';
+import { connect } from 'react-redux';
+import Avatar from 'material-ui/Avatar';
+import Assessment from 'material-ui/svg-icons/action/assessment';
+import Web from 'material-ui/svg-icons/av/web';
+import AudioActions from '../../store/actions/audioActions.js'
+import AudioList from '../../components/AudioList/AudioList'
+function mapStateToProps(state) {
+   console.log("state", state);
+    return {
+         audioList: state.epicReducer.audioList
+    };
+}
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+         fetchAudioData: function(){
+            return dispatch(AudioActions.getData())
+        }
+    };
+}
+
+class Navigation extends Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+  drawerMenu(){
+    return (
+      <div>
+          <div className="navigation-avatar-div">
+            <Avatar src="http://www.material-ui.com/images/uxceo-128.jpg"
+                    size={50}
+                    className="navigation-icon"/>
+          </div>
+          <MUI.MenuItem
+              className="navigation-menuItem"
+              primaryText="Dashboard"
+              leftIcon={<Assessment/>}
+              containerElement={<Link to="/dashboard"/>}
+            />
+          <MUI.MenuItem
+            className="navigation-menuItem"
+            primaryText="Users"
+            leftIcon={<Web/>}
+            containerElement={<Link to="/dashboard/userlist"/>}
+            /> 
+             <MUI.MenuItem
+            className="navigation-menuItem"
+            primaryText="Audio List"
+            leftIcon={<Web/>}
+            containerElement={<Link to="/dashboard/audioList"/>}
+          />
+          
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="navigation-container">
+        <MUI.AppBar style={this.props.styles} title="React Boilerplate"
+              onLeftIconButtonTouchTap={this.props.drawerToggle}
+              iconElementRight={<MUI.FlatButton label="Sign out" onTouchTap={this.props.logout}/>}
+              onRightIconButtonTouchTap={()=>this.context.router.push("/login")}
+              />
+        <MUI.Drawer open={this.props.drawerOpen} docked={false}
+            onRequestChange={this.props.drawerToggle}>
+          {this.drawerMenu()}
+        </MUI.Drawer>
+
+        <div>
+           <AudioList {...this.props}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Navigation);
